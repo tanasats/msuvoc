e2cd0a0a0587a0331bc4b76b8f1ed14a5576650c
Tanasat Sudjing <tanasat.s@msu.ac.th>
2025-02-27 05:56:32 +0700

    survey on iframe v 1.0

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -25,12 +25,14 @@ const Nav = ({ openNav }: Props) => {
         <div className={` ${navBg ? 'bg-blue-950 shadow-md' : 'fixed'} transition-all duration-200 h-[10vh] z-[1000] fixed w-full`}>
             <div className='flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto'>
                 {/* LOGO */}
-                <div className='flex items-center space-x-2'>
-                    <div className='w-10 h-10 bg-rose-500 rounded-full flex items-center justify-center flex-col'>
-                        <LuAudioLines className='w-6 h-6 text-white' />
+                <Link href={"/"}>
+                    <div className='flex items-center space-x-2'>
+                        <div className='w-10 h-10 bg-rose-500 rounded-full flex items-center justify-center flex-col'>
+                            <LuAudioLines className='w-6 h-6 text-white' />
+                        </div>
+                        <h1 className='text-xl md:text-3xl text-white font-bold'>VOC</h1>
                     </div>
-                    <h1 className='text-xl md:text-3xl text-white font-bold'>VOC</h1>
-                </div>
+                </Link>
                 {/* NavL inks */}
                 <div className='hidden lg:flex items-center space-x-10 text-base'>
                     {navLinks.map((link, index) => {
